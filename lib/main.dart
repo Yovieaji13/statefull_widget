0e8ad1b5326148708c586d0a2eb611d8dfda6cf0
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-02-25 16:33:25 +0700

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 // import 'package:flutter/services.dart';
 void main() {
  runApp(MyApp());
@@ -17,21 +18,62 @@ void main() {
           appBar: AppBar(
             title: Text("Konverter Suhu"),
         ),
+        
         body: Container(
           margin: EdgeInsets.all(8),
           child: Column(
             children: [
             TextFormField(
-                decoration: new InputDecoration(
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                  signed: false
+                ),
+
+                // controller: suhuController,
+                decoration: InputDecoration(
                   hintText: "Masukkan Suhu Dalam Celcius",
                 ),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9+\,]'))
+                ],
                 
-                validator: (value){
-                  if(value.isEmpty){
-                    return 'Harus inputkan angka';
-                }
-                return null;
-             },
+            //     validator: (value){
+            //       if(value.isEmpty){
+            //         return 'Harus inputkan angka';
+            //     }
+            //     return null;
+            //  },
+            ),
+
+            Expanded(
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Suhu dalam Kelvin"), Text("150")
+                    ],),
+                    margin: EdgeInsets.all(8),
+                  ),
+
+                  Container(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Suhu dalam Reamur"), Text("150")
+                    ],),
+                    margin: EdgeInsets.all(8),
+                  ),
+                ],
+              ) 
+            ),
+
+            RaisedButton(
+            color: Colors.blue,
+              onPressed: (){},
+              child: Text('Konversi Suhu', style: TextStyle(color: Colors.white),),
             ),
           ],)
         ),
